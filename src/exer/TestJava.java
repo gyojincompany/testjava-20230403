c1cb0bc1b6741f718b1ca007733ac125bbb9431c
gyojincompany <luckyred@gmail.com>
2023-04-03 16:18:07 +0900

    if문 연습

---
+++ src/exer/TestJava.java
@@ -0,0 +1,22 @@
+package exer;
+
+public class TestJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		final double CHANGE_RATE = 1309.5;
+		
+		
+		String str = "hello!!!"; 
+		int age = 15;
+		age = 20;
+		double weight = 57.7;
+		
+		age = (int) weight;//age = 57 임시적 형변환
+		boolean a = true;
+		
+		
+
+	}
+
+}
---
+++ src/exer/WonCalculator.java
@@ -0,0 +1,28 @@
+package exer;
+
+import java.util.Scanner;
+
+public class WonCalculator {
+
+	final double CHANGE_RATE = 1309.5; 
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("당신의 나이는 얼마입니까?");
+		Scanner scanner = new Scanner(System.in); 
+		//scanner 객체가 선언됨
+		
+		int student_age = scanner.nextInt();
+		
+		if(student_age >= 20){
+			System.out.println("당신은 성인이시군요!");
+		} else {
+			System.out.println("당신은 미성년이시군요!");
+		}
+		
+		System.out.println("당신의 나이는 " + student_age + "살 입니다.");
+		
+		scanner.close();//스캐너 객체 닫기
+	}
+
+}
